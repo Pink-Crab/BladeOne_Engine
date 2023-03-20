b18b8fec35d1917cadcab4039163b039cf0fd980
Glynn Quelch <glynn.quelch@gmail.com>
2023-03-20 00:46:10 +0000

    House Keeping

--- tests/files/views/testview.blade.php
+++ tests/files/views/testview.blade.php
@@ -1,6 +1,1 @@
 {{ $foo }}
-@form(method="post" action="testform")
-    @input(type="text" name="myform" value=$foo)
-    @textarea(name="description" value="default")
-    @button(text="click me" type="submit" class="test" onclick='alert("ok")')
-@endform
